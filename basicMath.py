0f29eea88134345e23812bd3c0c92e108c719629
SAURABHSALVE <saurabhsalve9999@gmail.com>
2025-04-26 11:45:22 +0530

    added code for summation

--- basicMath.py
+++ basicMath.py
@@ -39,8 +39,16 @@ if __name__ == "__main__":
   
    """
    
-   
-def factorial(n):
+   ## code to find the factorial of a number   
+# The factorial of a number n is the product of all positive integers less than or equal to n.
+# It is denoted by n! and is defined as:
+# n! = n * (n-1) * (n-2) * ... * 1
+# For example, 5! = 5 * 4 * 3 * 2 * 1 = 120 
+# The factorial of 0 is defined to be 1.
+# Factorial can be calculated using recursion or iteration.
+# Here is a simple recursive implementation of factorial in Python:
+
+"""def factorial(n):
     if n == 0 or n==1:
         return 1
     else:
@@ -48,6 +56,23 @@ def factorial(n):
     
 num = int(input("Enter a number: "))
 result = factorial(num)
-print(f"The factorial of {num} is {result}")
-        
+print(f"The factorial of {num} is {result}")"""
+
+
+## code to find the sum of digits of a number
+# The sum of digits of a number is the sum of all its individual digits.
+# For example, the sum of digits of 123 is 1 + 2 + 3 = 6.
+# The sum of digits can be calculated using a loop or recursion.
+# Here is a simple implementation of sum of digits in Python:
     
+"""def sum_of_digits(n):
+    sum = 0
+    while n > 0:
+        sum += n % 10
+        n //= 10
+    return sum
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    result = sum_of_digits(num)
+    print(f"The sum of digits of {num} is {result}")"""
+        
